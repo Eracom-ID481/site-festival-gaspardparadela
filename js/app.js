e2d8c35154a0b1127f39bd778b082c6a27ddcbf8
gaspardparadela <70263759+gaspardparadela@users.noreply.github.com>
2021-01-05 09:27:21 +0100

    Css + Ajout events

--- js/app.js
+++ js/app.js
@@ -4,101 +4,318 @@ let current = document.getElementsByClassName("active");
 drawTimetable(current[0].classList[1]);
 
 for (let e of filters) {
-  e.addEventListener("click", function () {
-    current = document.getElementsByClassName("active");
-    current[0].className = current[0].className.replace("active", "");
-    this.className += " active";
-    document.getElementById("timetable").innerHTML = "";
-    drawTimetable(current[0].classList[1]);
-  });
+    e.addEventListener("click", function() {
+        current = document.getElementsByClassName("active");
+        current[0].className = current[0].className.replace("active", "");
+        this.className += " active";
+        document.getElementById("timetable").innerHTML = "";
+        drawTimetable(current[0].classList[1]);
+    });
 }
 
 function drawTimetable(day) {
-  let timetable = new Timetable();
+    let timetable = new Timetable();
 
-  // On dessine une nouvelle grille selon le jour.
-  // Utilisez le nom de classe attribuée à chaque jour pour donner la condition
-  if (day == "vendredi") {
-    // Définir les heures de la grille (heure de début, heure de fin).
-    timetable.setScope(10, 00);
+    // On dessine une nouvelle grille selon le jour.
+    // Utilisez le nom de classe attribuée à chaque jour pour donner la condition
+    if (day == "vendredi") {
+        // Définir les heures de la grille (heure de début, heure de fin).
+        timetable.setScope(10, 00);
 
-    // Définir les lieux de la grille.
-    timetable.addLocations([
-      "Le Romandie",
-      "El Lokal",
-      "Bad Bonn",
-      "Rote Fabrik",
-    ]);
+        // Définir les lieux de la grille.
+        timetable.addLocations([
+            "Cinéma Rex",
+            "AS' Création",
+            "Musée de montreux",
+            "Théâtre Montreux-Riviera",
+            "Ned"
+        ]);
 
-    // Premier événement:
+        // Premier événement:
 
-    timetable.addEvent(
-      // Artiste
-      "Purple Mountains",
-      // Lieu
-      "El Lokal",
-      // Date et heure de début (année, mois, jour, heure, minute)
-      new Date(2020, 10, 17, 11, 20),
-      // Date et heure de fin (année, mois, jour, heure, minute)
-      new Date(2020, 10, 17, 13, 00),
-      // URL Quand on clicke sur le lien
-      { url: "../artiste.html" }
-    );
+        timetable.addEvent(
+            // Artiste
+            "Alien, le huitième passager",
+            // Lieu
+            "Cinéma Rex",
+            // Date et heure de début (année, mois, jour, heure, minute)
+            new Date(2021, 5, 7, 18, 30),
+            // Date et heure de fin (année, mois, jour, heure, minute)
+            new Date(2021, 5, 7, 20, 30),
+            // URL Quand on clicke sur le lien
+            {
+                url: "../artiste.html"
+            }
+        );
+        timetable.addEvent(
+            // Artiste
+            "Evil Dead 2",
+            // Lieu
+            "Cinéma Rex",
+            // Date et heure de début (année, mois, jour, heure, minute)
+            new Date(2021, 5, 7, 21, 00),
+            // Date et heure de fin (année, mois, jour, heure, minute)
+            new Date(2021, 5, 7, 22, 20),
+            // URL Quand on clicke sur le lien
+            {
+                url: "../artiste.html"
+            }
+        );
 
-    // Deuxième événement
-    timetable.addEvent(
-      "Bruce Springsteen",
-      "Bad Bonn",
-      new Date(2020, 10, 17, 13, 00),
-      new Date(2020, 10, 17, 15, 45),
-      { url: "../artiste.html" }
-    );
+        timetable.addEvent(
+            // Artiste
+            "Reverse Mountain",
+            // Lieu
+            "Ned",
+            // Date et heure de début (année, mois, jour, heure, minute)
+            new Date(2021, 5, 7, 22, 40),
+            // Date et heure de fin (année, mois, jour, heure, minute)
+            new Date(2021, 5, 7, 23, 30),
+            // URL Quand on clicke sur le lien
+            {
+                url: "../artiste.html"
+            }
+        );
 
-    let renderer = new Timetable.Renderer(timetable);
-    renderer.draw(".timetable");
-  }
-  // Deuxième jour
-  else if (day == "samedi") {
-    timetable.setScope(10, 00);
-    timetable.addLocations(["Les Docks", "Le Bourg", "PTR", "Fri-Son"]);
+        timetable.addEvent(
+            // Artiste
+            "Alt 236",
+            // Lieu
+            "Théâtre Montreux-Riviera",
+            // Date et heure de début (année, mois, jour, heure, minute)
+            new Date(2021, 5, 7, 17, 00),
+            // Date et heure de fin (année, mois, jour, heure, minute)
+            new Date(2021, 5, 7, 18, 30),
+            // URL Quand on clicke sur le lien
+            {
+                url: "../artiste.html"
+            }
+        );
 
-    timetable.addEvent(
-      "Booba",
-      "Le Bourg",
-      new Date(2020, 10, 17, 10, 20),
-      new Date(2020, 10, 17, 15, 30),
-      { url: "../artiste.html" }
-    );
+        timetable.addEvent(
+            // Artiste
+            "Ian Bedermann",
+            // Lieu
+            "AS' Création",
+            // Date et heure de début (année, mois, jour, heure, minute)
+            new Date(2021, 5, 7, 16, 00),
+            // Date et heure de fin (année, mois, jour, heure, minute)
+            new Date(2021, 5, 7, 20, 00),
+            // URL Quand on clicke sur le lien
+            {
+                url: "../artiste.html"
+            }
+        );
 
-    timetable.addEvent(
-      "Aphex Twin",
-      "PTR",
-      new Date(2020, 10, 17, 18, 30),
-      new Date(2020, 10, 17, 19, 45),
-      { url: "../artiste.html" }
-    );
-    let renderer = new Timetable.Renderer(timetable);
-    renderer.draw(".timetable");
-  }
-  // Troisième jour
-  else if (day == "dimanche") {
-    timetable.setScope(10, 00);
-    timetable.addLocations(["Les Docks", "Le Bourg", "PTR", "Fri-Son"]);
-    timetable.addEvent(
-      "The Mountain Goats",
-      "Le Bourg",
-      new Date(2020, 10, 17, 11, 00),
-      new Date(2020, 10, 17, 12, 00),
-      { url: "../artiste.html" }
-    );
-    timetable.addEvent(
-      "James McMurtry",
-      "PTR",
-      new Date(2020, 10, 17, 14, 00),
-      new Date(2020, 10, 17, 15, 00),
-      { url: "../artiste.html" }
-    );
-    let renderer = new Timetable.Renderer(timetable);
-    renderer.draw(".timetable");
-  }
+        timetable.addEvent(
+            // Artiste
+            "H.R. Giger",
+            // Lieu
+            "Musée de montreux",
+            // Date et heure de début (année, mois, jour, heure, minute)
+            new Date(2021, 5, 7, 10, 00),
+            // Date et heure de fin (année, mois, jour, heure, minute)
+            new Date(2021, 5, 7, 20, 00),
+            // URL Quand on clicke sur le lien
+            {
+                url: "../artiste.html"
+            }
+        );
+
+        let renderer = new Timetable.Renderer(timetable);
+        renderer.draw(".timetable");
+    }
+    // Deuxième jour
+    else if (day == "samedi") {
+        timetable.setScope(10, 00);
+        timetable.addLocations([
+            "Cinéma Rex",
+            "AS' Création",
+            "Musée de montreux",
+            "Ned"
+        ]);
+
+        timetable.addEvent(
+            "Deathgasm",
+            "Cinéma Rex",
+            new Date(2021, 5, 8, 12, 00),
+            new Date(2021, 5, 8, 13, 30), {
+                url: "../artiste.html"
+            }
+        );
+
+        timetable.addEvent(
+            "Grizzly Man",
+            "Cinéma Rex",
+            new Date(2021, 5, 8, 14, 00),
+            new Date(2021, 5, 8, 15, 40), {
+                url: "../artiste.html"
+            }
+        );
+
+        timetable.addEvent(
+            "Werner Herzog",
+            "Cinéma Rex",
+            new Date(2021, 5, 8, 15, 40),
+            new Date(2021, 5, 8, 16, 30), {
+                url: "../artiste.html"
+            }
+        );
+
+        timetable.addEvent(
+            "The Lighthouse",
+            "Cinéma Rex",
+            new Date(2021, 5, 8, 17, 00),
+            new Date(2021, 5, 8, 18, 30), {
+                url: "../artiste.html"
+            }
+        );
+
+        timetable.addEvent(
+            "Return of the living Dead",
+            "Cinéma Rex",
+            new Date(2021, 5, 8, 19, 00),
+            new Date(2021, 5, 8, 20, 30), {
+                url: "../artiste.html"
+            }
+        );
+
+        timetable.addEvent(
+            "Carpenter Brut",
+            "Ned",
+            new Date(2021, 5, 8, 21, 00),
+            new Date(2021, 5, 8, 22, 30), {
+                url: "../artiste.html"
+            }
+        );
+
+        timetable.addEvent(
+            // Artiste
+            "John Blanche",
+            // Lieu
+            "AS' Création",
+            // Date et heure de début (année, mois, jour, heure, minute)
+            new Date(2021, 5, 8, 16, 00),
+            // Date et heure de fin (année, mois, jour, heure, minute)
+            new Date(2021, 5, 8, 20, 00),
+            // URL Quand on clicke sur le lien
+            {
+                url: "../artiste.html"
+            }
+        );
+
+        timetable.addEvent(
+            // Artiste
+            "H.R. Giger",
+            // Lieu
+            "Musée de montreux",
+            // Date et heure de début (année, mois, jour, heure, minute)
+            new Date(2021, 5, 8, 10, 00),
+            // Date et heure de fin (année, mois, jour, heure, minute)
+            new Date(2021, 5, 8, 20, 00),
+            // URL Quand on clicke sur le lien
+            {
+                url: "../artiste.html"
+            }
+        );
+
+        let renderer = new Timetable.Renderer(timetable);
+        renderer.draw(".timetable");
+    }
+    // Troisième jour
+    else if (day == "dimanche") {
+        timetable.setScope(10, 00);
+        timetable.addLocations([
+            "Cinéma Rex",
+            "AS' Création",
+            "Musée de montreux",
+            "Ned"
+        ]);
+
+        timetable.addEvent(
+            "Commando Ninja",
+            "Cinéma Rex",
+            new Date(2021, 5, 9, 13, 00),
+            new Date(2021, 5, 9, 14, 00), {
+                url: "../artiste.html"
+            }
+        );
+        timetable.addEvent(
+            "Jodorowsky's Dune",
+            "Cinéma Rex",
+            new Date(2021, 5, 9, 14, 30),
+            new Date(2021, 5, 9, 15, 00), {
+                url: "../artiste.html"
+            }
+        );
+
+        timetable.addEvent(
+            "Alejandro Jodorowsky",
+            "Cinéma Rex",
+            new Date(2021, 5, 9, 14, 30),
+            new Date(2021, 5, 9, 15, 20), {
+                url: "../artiste.html"
+            }
+        );
+
+        timetable.addEvent(
+            "Lifeforce",
+            "Cinéma Rex",
+            new Date(2021, 5, 9, 15, 50),
+            new Date(2021, 5, 9, 16, 40), {
+                url: "../artiste.html"
+            }
+        );
+
+        timetable.addEvent(
+            "Hard To Be A God",
+            "Cinéma Rex",
+            new Date(2021, 5, 9, 17, 00),
+            new Date(2021, 5, 9, 19, 50), {
+                url: "../artiste.html"
+            }
+        );
+
+        timetable.addEvent(
+            "Mars Red Sky",
+            "Ned",
+            new Date(2021, 5, 9, 20, 00),
+            new Date(2021, 5, 9, 21, 20), {
+                url: "../artiste.html"
+            }
+        );
+
+        timetable.addEvent(
+            // Artiste
+            "John Blanche",
+            // Lieu
+            "AS' Création",
+            // Date et heure de début (année, mois, jour, heure, minute)
+            new Date(2021, 5, 8, 16, 00),
+            // Date et heure de fin (année, mois, jour, heure, minute)
+            new Date(2021, 5, 8, 20, 00),
+            // URL Quand on clicke sur le lien
+            {
+                url: "../artiste.html"
+            }
+        );
+
+        timetable.addEvent(
+            // Artiste
+            "H.R. Giger",
+            // Lieu
+            "Musée de montreux",
+            // Date et heure de début (année, mois, jour, heure, minute)
+            new Date(2021, 5, 9, 10, 00),
+            // Date et heure de fin (année, mois, jour, heure, minute)
+            new Date(2021, 5, 9, 20, 00),
+            // URL Quand on clicke sur le lien
+            {
+                url: "../artiste.html"
+            }
+        );
+
+        let renderer = new Timetable.Renderer(timetable);
+        renderer.draw(".timetable");
+    }
 }
